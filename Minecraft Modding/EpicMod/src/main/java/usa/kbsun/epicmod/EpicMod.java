a03a551faa6f0b74a25edae0434bb005676c84dc
kmann383 <yes.com@yes.com>
2021-03-03 16:58:42 -0800

    mhm

---
+++ Minecraft Modding/EpicMod/src/main/java/usa/kbsun/epicmod/EpicMod.java
@@ -0,0 +1,11 @@
+package usa.kbsun.epicmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EpicMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
